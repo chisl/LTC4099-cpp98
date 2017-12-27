c80c19c649b707fa614e58dd1c18f0da683223c5
chisl <info@chisl.io>
2017-12-27 18:14:41 +0100

    Initial commit

---
+++ LTC4099.cpp
@@ -0,0 +1,13 @@
+/*
+ * name:        LTC4099
+ * description: I2C Controlled USB Power Manager/Charger with Overvoltage Protection
+ * manuf:       Linear Technology
+ * version:     0.1
+ * url:         http://cds.linear.com/docs/en/datasheet/4099fd.pdf
+ * date:        2017-12-20
+ * author       https://chisl.io/
+ * file:        LTC4099.cpp
+ */
+
+#include "LTC4099.hpp"
+
